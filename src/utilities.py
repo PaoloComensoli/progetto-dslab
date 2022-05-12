f4a02cb1b01754510d7c894ce21e97d0acc82bf1
maurizio-papa <mauriziopapa52@gmail.com>
2022-05-12 12:23:46 +0200

    add graphs

--- src/utilities.py
+++ src/utilities.py
@@ -1,11 +1,14 @@
 import sklearn
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def associate_sector_to_category(cluster_predicted, n_clusters):
+
+def associate_sector_to_category(cluster_predicted, n_clusters, df):
     """
-    associates each time series to its own cluster"""
+    associates each time series to its own cluster
+    """
     clusters = {}
     category = [element for element in set(df['settore'])]
     for n_cluster in range(n_clusters):
@@ -19,13 +22,16 @@ def associate_sector_to_category(cluster_predicted, n_clusters):
     return clusters
 
 
-def plot_clusters(n_clusters, fitted_model):
+def plot_clusters(n_clusters, fitted_model, processed_df):
     """
     plots the obtained clusters 
     """
     fig, ax = plt.subplots(n_clusters,1, figsize =(20,15))
     for clusters in range(n_clusters):
         ax[clusters].plot(processed_df.data,fitted_model.cluster_centers_[clusters])
+    return fig
+
+
 
 
 def pivot_scale_and_fillna(df):
@@ -40,5 +46,8 @@ def pivot_scale_and_fillna(df):
     df_pivoted = df_pivoted.fillna(0)
     df_pivoted = pd.DataFrame(scaler.fit_transform(df_pivoted), columns = col_names, index = index_)
     return df_pivoted
+
+
+
 
 
